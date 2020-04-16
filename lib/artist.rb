ff3cacf9eb82546a152ef0de3c2d7bbce2429c6c
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:15:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,27 @@
+
+require_relative 'song'
+
 class Artist
-  attr_accessor :name, :songs 
-  
-  @@song_count = 0
-  
-  def self.song_count
-    @@song_count
-  end
-  
+  attr_accessor :name, :songs
+
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
-    @@song_count += 1 
-  end 
-  
+  end
+
   def add_song(song)
+    song.artist = self
     self.songs << song
-    song.artist = self 
-    @@song_count += 1 
-  end 
-  
+  end
+
   def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song 
-    song.artist = self 
-    @@song_count += 1 
-  end 
-  
-  def songs 
-    @songs
-  end 
-  
+    s = Song.new(name)
+    s.artist = self
+    self.songs << s
+  end
+
+  def self.song_count
+    Song.all.length
+  end
+
 end
-
-Artist.song_count
-
